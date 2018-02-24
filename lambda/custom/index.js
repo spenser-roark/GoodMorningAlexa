69a9260a7904dfc27524495ca2955ed120e1a49d
Spenser Roark <red.arremer.ace@gmail.com>
2018-02-24 15:39:08 +0000

    Base speech response and default hello world!

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -1,4 +1,33 @@
+function buildSpeechletResponse(title, output, repromptText, shouldEndSession) {
+	return {
+		outputSpeech: {
+			type: 'PlainText',
+			text: output
+		},
+		card: {
+			type: 'Simple',
+			title: `SessionSpeechlet - ${title}`,
+			content: `SessionSpeechlet - ${output}`
+		},
+		reprompt: {
+			outputSpeech: {
+				type: 'PlainText',
+				text: repromptText
+			}
+		},
+		shouldEndSession
+	};
+}
+
+function buildResponse(sessionAttributes, speechletResponse) {
+	return {
+		version: '1.0',
+		sessionAttributes,
+		response: speechletResponse
+	};
+}
+
 exports.handler = (event, context, callback) => {
-    // TODO implement
-    callback(null, 'Hello from Lambda');
+	let sessionAttributes = {};
+    callback(null, buildResponse(sessionAttributes, buildSpeechletResponse("Test", "Hello World", null, true)));
 };
